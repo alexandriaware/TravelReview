05e294bec967144dffb3f0cfd73cb9a7c74f1d6b
alexandriaware <ali.mercer92@gmail.com>
2017-10-27 13:01:56 -0400

    added review properties

--- TravelReview/Models/Review.cs
+++ TravelReview/Models/Review.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +9,14 @@ namespace TravelReview.Models
 {
     public class Review
     {
+        [Key]
+        public int ReviewID { get; set; }
+        public string ReviewTitle { get; set; }
+        public string ReviewContent { get; set; }
+        public int Stars { get; set; }
+
+        [ForeignKey("Category")]
+        public int CategoryID { get; set; }
+        public virtual Category Category { get; set; }
     }
 }
